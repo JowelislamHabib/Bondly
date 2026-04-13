b1013c1106b2e6150f0702e246f30fe822816def
JowelislamHabib <jowel@bintofajjal.com>
2026-04-13 20:29:31 +0600

    feat: Add NavBar component and update styles; include lucide-react dependency

---
+++ src/app/timeline/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TimelinePage = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>timeline</h1>
+    </div>
+  );
+};
+
+export default TimelinePage;
