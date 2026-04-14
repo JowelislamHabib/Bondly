07c9b3e30e2ad77da4133158bc079e0089adc7f4
JowelislamHabib <jowel@bintofajjal.com>
2026-04-14 12:48:47 +0600

    feat: Update FriendsList and add FriendsCard component for dynamic friend display; enhance friend status representation

---
+++ src/app/components/Home/FriendsCard.jsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React from "react";
+
+const FriendsCard = (friend) => {
+  const relationStatus = {
+    active: "bg-green-600 text-white border-green-700",
+    "strong-connection": "bg-emerald-600 text-white border-emerald-700",
+    "recently-contacted": "bg-sky-600 text-white border-sky-700",
+    "client-active": "bg-blue-600 text-white border-blue-700",
+    occasional: "bg-indigo-600 text-white border-indigo-700",
+    "at-risk": "bg-yellow-500 text-black border-yellow-600",
+    overdue: "bg-red-600 text-white border-red-700",
+    dormant: "bg-gray-600 text-white border-gray-700",
+  };
+  console.log(friend.friend);
+  const myFriend = friend.friend;
+  const { name, picture, tags } = myFriend;
+  const checkStatus = myFriend.status.toLowerCase();
+
+  return (
+    <div>
+      <div>
+        <div className="card bg-base-100 shadow-sm p-6">
+          <figure>
+            <Image
+              src={picture}
+              alt="name"
+              height={80}
+              width={80}
+              className="rounded-full shadow-md border-2 border-neutral-100 h-30 w-30 object-fit-cover mb-3"
+            />
+          </figure>
+          <div className="flex flex-col gap-2 items-center text-center">
+            <h2 className="text-xl font-semibold">{name}</h2>
+            <small>62d ago</small>
+            <div className="flex gap-2">
+              {tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="uppercase px-4 py-1 rounded-full text-xs text-green-700 font-medium bg-green-200"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <p
+              className={`px-4 py-2 rounded-full text-sm font-medium ${relationStatus[checkStatus] || "bg-gray-100 text-gray-700 border border-gray-200"}`}
+            >
+              <span className="capitalize">{checkStatus}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FriendsCard;
--- src/app/components/Home/FriendsList.jsx
+++ src/app/components/Home/FriendsList.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import FriendsCard from "./FriendsCard";
 
-const FriendsList = () => {
+const FriendsList = async () => {
+  const response = await fetch("http://localhost:3000/friends.json");
+  const friends = await response.json();
+  // console.log(friends);
+
   return (
     <div className="container mx-auto px-4">
       <div>
@@ -8,25 +13,9 @@ const FriendsList = () => {
       </div>
 
       <div className="grid md:grid-col-2 lg:grid-cols-4 gap-4 mt-4">
-        <div className="card bg-base-100 shadow-sm p-6">
-          <figure className="">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-full h-20 w-20 object-cover"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="text-xl font-semibold">David Kim</h2>
-            <small>62d ago</small>
-            <p className="uppercase px-4 py-2 rounded-full text-xs font-medium bg-green-200">
-              work
-            </p>
-            <p className="capital px-4 py-2 rounded-full text-xs font-medium bg-orange-300">
-              Almost Due
-            </p>
-          </div>
-        </div>
+        {friends.map((friend) => (
+          <FriendsCard key={friend.id} friend={friend} />
+        ))}
       </div>
     </div>
   );
