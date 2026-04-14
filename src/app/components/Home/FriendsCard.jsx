689f12ab5d5911f4da1276c3bbc57682cf2c1723
JowelislamHabib <jowel@bintofajjal.com>
2026-04-14 13:05:22 +0600

    feat: Wrap FriendsCard content in a Link component for improved navigation; maintain existing layout and styling

--- src/app/components/Home/FriendsCard.jsx
+++ src/app/components/Home/FriendsCard.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FriendsCard = (friend) => {
@@ -20,36 +21,38 @@ const FriendsCard = (friend) => {
   return (
     <div>
       <div>
-        <div className="card bg-base-100 shadow-sm p-6">
-          <figure>
-            <Image
-              src={picture}
-              alt="name"
-              height={80}
-              width={80}
-              className="rounded-full shadow-md border-2 border-neutral-100 h-30 w-30 object-fit-cover mb-3"
-            />
-          </figure>
-          <div className="flex flex-col gap-2 items-center text-center">
-            <h2 className="text-xl font-semibold">{name}</h2>
-            <small>62d ago</small>
-            <div className="flex gap-2">
-              {tags.map((tag, index) => (
-                <span
-                  key={index}
-                  className="uppercase px-4 py-1 rounded-full text-xs text-green-700 font-medium bg-green-200"
-                >
-                  {tag}
-                </span>
-              ))}
+        <Link href={"/"}>
+          <div className="card bg-base-100 shadow-sm p-6">
+            <figure>
+              <Image
+                src={picture}
+                alt="name"
+                height={80}
+                width={80}
+                className="rounded-full shadow-md border-2 border-neutral-100 h-30 w-30 object-fit-cover mb-3"
+              />
+            </figure>
+            <div className="flex flex-col gap-2 items-center text-center">
+              <h2 className="text-xl font-semibold">{name}</h2>
+              <small>62d ago</small>
+              <div className="flex gap-2">
+                {tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="uppercase px-4 py-1 rounded-full text-xs text-green-700 font-medium bg-green-200"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <p
+                className={`px-4 py-2 rounded-full text-sm font-medium ${relationStatus[checkStatus] || "bg-gray-100 text-gray-700 border border-gray-200"}`}
+              >
+                <span className="capitalize">{checkStatus}</span>
+              </p>
             </div>
-            <p
-              className={`px-4 py-2 rounded-full text-sm font-medium ${relationStatus[checkStatus] || "bg-gray-100 text-gray-700 border border-gray-200"}`}
-            >
-              <span className="capitalize">{checkStatus}</span>
-            </p>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
